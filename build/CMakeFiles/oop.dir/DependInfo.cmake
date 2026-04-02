43ae0b2d4453d58feb234da0f3b2954a31d9005a
Daniel <lacatusudaniel67@gmail.com>
2026-04-02 21:46:21 +0300

    update

---
+++ build/CMakeFiles/oop.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/AbonamentVIP.cpp" "CMakeFiles/oop.dir/AbonamentVIP.cpp.obj" "gcc" "CMakeFiles/oop.dir/AbonamentVIP.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/AgentieTicketing.cpp" "CMakeFiles/oop.dir/AgentieTicketing.cpp.obj" "gcc" "CMakeFiles/oop.dir/AgentieTicketing.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/Bilet.cpp" "CMakeFiles/oop.dir/Bilet.cpp.obj" "gcc" "CMakeFiles/oop.dir/Bilet.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/BiletAbonament.cpp" "CMakeFiles/oop.dir/BiletAbonament.cpp.obj" "gcc" "CMakeFiles/oop.dir/BiletAbonament.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/BiletRedus.cpp" "CMakeFiles/oop.dir/BiletRedus.cpp.obj" "gcc" "CMakeFiles/oop.dir/BiletRedus.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/BiletStandard.cpp" "CMakeFiles/oop.dir/BiletStandard.cpp.obj" "gcc" "CMakeFiles/oop.dir/BiletStandard.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/BiletVIP.cpp" "CMakeFiles/oop.dir/BiletVIP.cpp.obj" "gcc" "CMakeFiles/oop.dir/BiletVIP.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/Client.cpp" "CMakeFiles/oop.dir/Client.cpp.obj" "gcc" "CMakeFiles/oop.dir/Client.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/Eveniment.cpp" "CMakeFiles/oop.dir/Eveniment.cpp.obj" "gcc" "CMakeFiles/oop.dir/Eveniment.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/ExceptieTicketing.cpp" "CMakeFiles/oop.dir/ExceptieTicketing.cpp.obj" "gcc" "CMakeFiles/oop.dir/ExceptieTicketing.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/Locatie.cpp" "CMakeFiles/oop.dir/Locatie.cpp.obj" "gcc" "CMakeFiles/oop.dir/Locatie.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/Meniu.cpp" "CMakeFiles/oop.dir/Meniu.cpp.obj" "gcc" "CMakeFiles/oop.dir/Meniu.cpp.obj.d"
+  "C:/Users/lacat/Desktop/SEMESTRUL 2/POO/templateProiectPOO/main.cpp" "CMakeFiles/oop.dir/main.cpp.obj" "gcc" "CMakeFiles/oop.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
